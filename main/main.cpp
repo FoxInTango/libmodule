8eaaf5cbe2cedf5211d1471490d3000df6fcaa4f
李诗军 <foxintango@yeah.net>
2021-07-14 20:38:09 +0800

    null

--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,13 @@
 #include <libmodule/libmodule.h>
 using namespace foxintango;
+#include <iostream>
 
 int main(int argc,char** argv) {
     Module* module = new Module("IOEngineLWS.so");
 
     ModuleInterface* interface = module->interface();
     if(interface) {
-        std::cout << "module IOEngineLWS loaded " << std:endl;
+        std::cout << "module IOEngineLWS loaded " << std::endl;
     }
     return 0;
 }
