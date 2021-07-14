b0a3d351adf7f759f636041ec9278aadd52618ef
李诗军 <foxintango@yeah.net>
2021-07-14 20:34:46 +0800

    null

--- main/main.cpp
+++ main/main.cpp
@@ -5,5 +5,8 @@ int main(int argc,char** argv) {
     Module* module = new Module("IOEngineLWS.so");
 
     ModuleInterface* interface = module->interface();
+    if(interface) {
+        std::cout << "module IOEngineLWS loaded " << std:endl;
+    }
     return 0;
 }
