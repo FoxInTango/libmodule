9e16b248973b2ebb9e9d695226cfcc096ea22c85
李诗军 <foxintango@yeah.net>
2021-07-14 17:08:29 +0800

    null

--- include/ModuleInterface.h
+++ include/ModuleInterface.h
@@ -1,6 +1,7 @@
 #ifndef _MODULE_INTERFACE_H_
 #define _MODULE_INTERFACE_H_
 
+#include <libmodel/libmodel.h>
 #include <libcpp/libcpp.h>
 
 namespace foxintango {
@@ -12,8 +13,12 @@ public:
 public:
     virtual bool  containClass(const unsigned char* name) = 0;
     virtual void* createObject(const unsigned char* name) = 0;
+    virtual void* createObject(const unsigned char* name,const Model& model) = 0;
+
     virtual bool  containFunction(const unsigned char* name) = 0;
     virtual void* exportFunction(const unsigned char* name) = 0;
+    
+    virtual void* exportVariable(const unsigned char* name) = 0;
 };
 }
 #endif
