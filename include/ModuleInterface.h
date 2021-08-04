8d57b6ee79a5efe1666a0449e02aa9a242b96864
sparrow.wsl <foxintango@hotmail.com>
2021-08-04 21:07:25 +0800

    NOTHING

--- include/ModuleInterface.h
+++ include/ModuleInterface.h
@@ -1,3 +1,26 @@
+/*
+ * libmodule
+ *
+ * Copyright (C) 2021 FoxInTango <foxintango@yeah.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 #ifndef _MODULE_INTERFACE_H_
 #define _MODULE_INTERFACE_H_
 
@@ -10,14 +33,14 @@ public:
     ModuleInterface();
     virtual ~ModuleInterface();
 public:
-    virtual bool  containClass(const unsigned char* name) = 0;
-    virtual void* createObject(const unsigned char* name) = 0;
-    virtual void* createObject(const unsigned char* name,const Model& model) = 0;
+    virtual bool  containClass(const char* name) = 0;
+    virtual void* createObject(const char* name) = 0;
+    virtual void* createObject(const char* name,const Model& model) = 0;
 
-    virtual bool  containFunction(const unsigned char* name) = 0;
-    virtual void* exportFunction(const unsigned char* name) = 0;
+    virtual bool  containFunction(const char* name) = 0;
+    virtual void* exportFunction(const char* name) = 0;
     
-    virtual void* exportVariable(const unsigned char* name) = 0;
+    virtual void* exportVariable(const char* name) = 0;
 };
 }
 #endif
