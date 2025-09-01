25ef504ea78edc9f46967282c4f644a6c0581768
sparrow <sparrow@wsl.me>
2025-09-01 22:29:33 +0800

    Mon Sep  1 22:29:33 CST 2025

--- .make/depend.dir/libfs.mk
+++ .make/depend.dir/libfs.mk
@@ -1,9 +1,9 @@
-DEPEND_TARGETS += libfs.build
+DEPEND_TARGETS += libfs.recursive
 ECHO_TARGETS += libfs.echo
-libfs.build:
+libfs.recursive:
 	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libfs/.make/super
 	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libfs/.make/super
-	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libfs/ && make && make install
+	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libfs/ && make recursive && make install
 	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libfs/.make/super
 libfs.echo:
 	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libfs/.make/super
--- .make/depend.dir/libmodel.mk
+++ .make/depend.dir/libmodel.mk
@@ -1,9 +1,9 @@
-DEPEND_TARGETS += libmodel.build
+DEPEND_TARGETS += libmodel.recursive
 ECHO_TARGETS += libmodel.echo
-libmodel.build:
+libmodel.recursive:
 	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodel/.make/super
 	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodel/.make/super
-	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodel/ && make && make install
+	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodel/ && make recursive && make install
 	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodel/.make/super
 libmodel.echo:
 	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodule/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libmodel/.make/super
